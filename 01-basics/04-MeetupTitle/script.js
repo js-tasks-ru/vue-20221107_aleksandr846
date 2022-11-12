05dcfeeb883aa5b101e9be4845ae67d93034f01c
AleksandrZ <forros@mail.ru>
2022-11-12 18:59:43 +0700

    вывод заголовка Meetup'а

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+ import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,24 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+const App = defineComponent({
+  name: 'App',
+  data() {
+      return {
+          selected_radio: '',
+          meetupTitle: ''
+      }
+  },
+  watch: {
+    selected_radio(newValue, oldValue) {
+        fetchMeetupById(newValue)
+        .then((response) => {
+          this.meetupTitle = response.title;
+        });
+      }
+  }
+});
+
+const app = createApp(App);
+const vm = app.mount('#app');
+window.vm = vm;
